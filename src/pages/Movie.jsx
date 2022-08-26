41fb266ff3928bc58b7fd3e3cb09b0dbbc55dedb
f01yy <79443616+f01yy@users.noreply.github.com>
2022-08-26 20:04:04 +0400

    .

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -21,54 +21,15 @@ const Movie = () => {
   return (
     <article>
       <div className='movie-page'>
-        {isLoading ? (
-          <Loader />
-        ) : (
-          <main>
-            <h1 className='movie-page__title'>
-              {movie.title ? movie.title_long : ''}
-            </h1>
-            <div className='movie-page__content'>
-              <div className='movie-page__img'>
-                <img
-                  src={movie.medium_cover_image}
-                  alt={movie.slug ? movie.slug : ''}
-                />
-              </div>
-              <div className='movie-page__text'>
-                <div className='movie-page__extra'>
-                  {movie.mpa_rating ? (
-                    <span className='movie-page__mpa'>{movie.mpa_rating}</span>
-                  ) : (
-                    ''
-                  )}
-                  {movie.rating ? (
-                    <span className='movie-page__rating'>
-                      {`IMDb: ${movie.rating}`}
-                    </span>
-                  ) : (
-                    ''
-                  )}
-                  {movie.runtime ? (
-                    <span className='movie-page__runtime'>
-                      {`${movie.runtime} min`}
-                    </span>
-                  ) : (
-                    ''
-                  )}
-                </div>
-                <div className='movie-page__genres'>
-                  {movie.genres
-                    ? movie.genres.map((genre) => (
-                        <span key={genre}>{genre}</span>
-                      ))
-                    : ''}
-                </div>
-                <p className='movie-page__desc'>{movie.description_full}</p>
-              </div>
-            </div>
-          </main>
-        )}
+        <div className='movie-page__container'>
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <main>
+              
+            </main>
+          )}
+        </div>
       </div>
     </article>
   );
